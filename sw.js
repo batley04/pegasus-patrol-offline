7d94bcc967eb250af34c0c3bb3d8077c1b971031
Jamie Batley <jbatley01@gmail.com>
2026-08-24 00:22:51 +0100

    Build 008 - Add offline QR decoder

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // ======================================================
 
 const CACHE_NAME =
-  "pegasus-patrol-build-008-v14";
+  "pegasus-patrol-build-008-v15";
 
 const APP_FILES = [
 
@@ -14,6 +14,7 @@ const APP_FILES = [
   "./db.js",
   "./sync.js",
   "./manifest.json"
+  "./lib/jsQR.js",
 
 ];
 
